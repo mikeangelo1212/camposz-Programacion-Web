9bf805cbb46147e22e168fab6fe0db0373bd2943
= <=>
2022-06-01 14:46:52 -0500

    Eliminacion de comentarios

--- EjercicioFinal/PHP/consultar.php
+++ EjercicioFinal/PHP/consultar.php
@@ -1,15 +1,13 @@
 <?php
 $idEmpleado=$_POST['id'];
-//$iduser=1;
 $hostname='localhost';
 $database='l19100154';
 $username='root';
 $password='';
 $port='3306';
 
-//echo $iduser;
+
 try {
-    //$con = new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
     $con = new PDO("mysql:host=$hostname;dbname=$database;port=$port",$username,$password);
 } catch(PDOException $e) {
     echo "Error de conexion a la base de datos";
